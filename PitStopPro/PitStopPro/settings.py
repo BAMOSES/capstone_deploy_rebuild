4732a2b1021624377a0bbeaf0fcb9941245f3134
Blaise Moses <70296155+BAMOSES@users.noreply.github.com>
2024-04-23 03:37:10 -0400

    Update settings.py

--- PitStopPro/PitStopPro/settings.py
+++ PitStopPro/PitStopPro/settings.py
@@ -25,14 +25,14 @@ TWILIO_PHONE_NUMBER=os.environ.get('+18334091034')
 SECRET_KEY = "django-insecure-z$t&)5_xv&5u58igz6%h7ghkcz3r18e7$)i=*2!5r#7er+m&9a"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['capstonedeployrebuild-production.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = [‘https://*.railway.app’, '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['capstonedeployrebuild-production.up.railway.app']
+CSRF_TRUSTED_ORIGINS = [‘https://*.railway.app’]
 
 CORS_ALLOWED_ORIGINS = [
-    'capstonedeployrebuild-production.up.railway.app'
+    '‘https://*.railway.app’'
 ]
 
 
