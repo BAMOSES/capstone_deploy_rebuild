c929b4a2533f0b5c3a7dbfb8eda799cf7cef618c
Blaise Moses <70296155+BAMOSES@users.noreply.github.com>
2024-04-23 03:20:19 -0400

    Update settings.py

--- PitStopPro/PitStopPro/settings.py
+++ PitStopPro/PitStopPro/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = "django-insecure-z$t&)5_xv&5u58igz6%h7ghkcz3r18e7$)i=*2!5r#7er+m&9a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['capstonedeployrebuild-production.up.railway.app', 'http://127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['capstonedeployrebuild-production.up.railway.app']
 
 
 # Application definition
